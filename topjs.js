883c6651e36e75dd5b8fdeaca5972dd0d01ffa77
Cheikh Oumou <67443473+cheikhoumou@users.noreply.github.com>
2022-08-23 07:53:40 -0700

    v1
    v1

---
+++ topjs.js
@@ -0,0 +1,77 @@
+document.querySelector('header.site-header + section[style="background: #FAFAFA;"] + section').innerHTML = `<div class="contss">
+<style>
+::-webkit-scrollbar {
+    width: 1px;
+    height: 4px;
+}::-webkit-scrollbar-thumb {
+    background: #40798c;
+    border-radius: 10px;
+}::-webkit-scrollbar-track {
+    border: 1px solid #ffffff;
+}.box-cheikho{
+    display: grid;
+    grid-template-columns: repeat(10,1fr);
+    overflow: auto;
+    width: 94%;
+    margin:8px auto;
+}.box-cheikho span {
+    height: 170px;
+    width: 105px;
+    display: block;
+    margin: 0px 5px;
+}.box-cheikho span img {
+    width: 100%;
+    height: fit-content;
+}.text-code {
+    width: 50%;
+    margin: auto;
+    padding: 15px;
+}.text-code img {
+    width: 100%;
+}.flex-cheikho {
+    display: flex;
+    width: 95%;
+    margin: 10px auto;
+    justify-content: space-between;
+}.div-ch a img {
+    width: 100%;
+    box-shadow: 0px 0px 16px #1704046b;
+    border-radius: 20px;
+}.flex-cheikho .div-ch {
+    width: 48.5%;
+}.box-cheikho span img:hover {
+    transition: all 0.6s;
+    filter: brightness(0.7);
+}
+</style>
+<!-- html code start -->
+<div class="text-code"><img src="https://www10.0zz0.com/2022/08/23/14/716213697.png" alt=""></div>
+<div class="box-cheikho">
+    <span>
+        <a href="https://cardsworldsa.com/شدات-ببجي-موبايل/c971569283"> <img src="https://www4.0zz0.com/2022/08/23/12/142592597.png"/></a>
+    </span>
+    <span>
+       <a href="https://cardsworldsa.com/شدات-ببجي-نيوستيت/c196413324"> <img src="https://www4.0zz0.com/2022/08/23/12/957950101.png"/></a> 
+    </span>    <span>
+       <a href="https://cardsworldsa.com/مجوهرات-أزار/c1704076941"> <img src="https://www4.0zz0.com/2022/08/23/12/645104457.png"/></a> 
+    </span>    <span>
+       <a href="https://cardsworldsa.com/الماس-وذهب-يلا-لودو/c931087758"> <img src="https://www4.0zz0.com/2022/08/23/12/722012922.png"/></a> 
+    </span>    <span>
+       <a href="https://cardsworldsa.com/ذهب-يلا-لابف/c21259407"><img src="https://www4.0zz0.com/2022/08/23/12/925031819.png"/> </a> 
+    </span>    <span>
+        <a href="https://cardsworldsa.com/عملات-تبك-توك/c1394770824"><img src="https://www4.0zz0.com/2022/08/23/12/337347281.png"/> </a>
+    </span>    <span>
+       <a href="https://cardsworldsa.com/الماس-بيقو-لايف/c755999369"> <img src="https://www4.0zz0.com/2022/08/23/12/180372282.png"/></a> 
+    </span>    <span>
+       <a href="https://cardsworldsa.com/جواهر/c1219616907"><img src="https://www4.0zz0.com/2022/08/23/12/293036941.png"/> </a> 
+    </span>    <span>
+       <a href="https://cardsworldsa.com/أشتراك-نتفلكس/c446566292">  <img src="https://www4.0zz0.com/2022/08/23/12/104304070.png"/></a>
+    </span>    <span>
+      <a href="https://cardsworldsa.com/يوتيوب-بريميوم/c1954229909">  <img src="https://www4.0zz0.com/2022/08/23/12/477249387.png"/></a> 
+    </span> 
+</div>
+<div class="flex-cheikho">
+    <div class="div-ch"><a href="https://cardsworldsa.com/offers"><img src="https://www8.0zz0.com/2022/08/23/13/245557273.png" alt=""></a></div>
+    <div class="div-ch"><a href="https://cardsworldsa.com/%D8%A8%D8%B7%D8%A7%D9%82%D8%A9-%D8%B9%D8%A7%D9%84%D9%85-%D8%A7%D9%84%D8%A8%D8%B7%D8%A7%D9%82%D8%A7%D8%AA/c2072745788"><img src="https://www8.0zz0.com/2022/08/23/13/820444554.png" alt=""></a></div>
+</div>
+</div>`;
