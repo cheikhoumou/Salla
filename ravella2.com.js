18a0f9f16aeac5cd4ca3526c389771c28a34ec61
Cheikh Oumou <67443473+cheikhoumou@users.noreply.github.com>
2022-09-06 16:08:55 -0700

    v1
    v1

--- ravella2.com.js
+++ ravella2.com.js
@@ -1,1 +1,216 @@
-console.log('cheikho code activet 1');
+document.querySelector(
+  'header.site-header + section[style="background: #FAFAFA;"] + section'
+).innerHTML = `
+<style>
+    body{
+        direction: rtl;
+    }
+  ::-webkit-scrollbar {
+    width: 1px;
+    height: 4px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: linear-gradient(45deg, #d3ab56, #b1752d);
+    border-radius: 10px;
+  }
+  ::-webkit-scrollbar-track {
+    border: 1px solid #ffffff;
+  }
+  .box-cheikho {
+    display: grid;
+    grid-template-columns: repeat(10, 1fr);
+    overflow: auto;
+    width: 94%;
+    margin: 8px auto;
+  }
+  .box-cheikho span {
+    height: 170px;
+    width: 105px;
+    display: block;
+    margin: 0px 5px;
+  }
+  .box-cheikho span img {
+    width: 100%;
+    height: 92%;
+  }
+  .text-code {
+    width: 80%;
+    margin: auto;
+    padding: 15px;
+  }
+  .text-code img {
+    width: 100%;
+  }
+  .flex-cheikho {
+    display: flex;
+    width: 95%;
+    margin: 10px auto;
+    justify-content: space-between;
+  }
+  .div-ch a img {
+    width: 100%;
+    box-shadow: 0px 0px 16px #1704046b;
+    border-radius: 20px;
+  }
+  .flex-cheikho .div-ch {
+    width: 48.5%;
+  }
+  .box-cheikho span img:hover {
+    transition: all 0.6s;
+    filter: brightness(0.7);
+  }
+  @media (max-width: 990px) {
+    .list-home a img {
+      width: 90%;
+      height: 100%;
+    }
+    .list-home a {
+      margin: 2% 3%;
+      margin-bottom: 0;
+    }
+    .list-home {
+      display: flex !important;
+      width: 100%;
+      position: fixed;
+      z-index: 999999;
+      bottom: 0;
+      justify-content: center;
+      align-items: center;
+      background: #40798c;
+      direction: ltr;
+      left: 0;
+      right: 0;
+      box-shadow: -2px -2px 8px #1c070730;
+    }
+  }
+  .list-home {
+    display: none;
+  }
+  a.sub-nav__menu.ml-2 svg {
+    display: none;
+  }
+  .flex-cheikho.cl .div-ch {
+    width: 32%;
+  }
+  .flex-cheikho.cl .div-ch a img {
+    border-radius: 8px;
+  }
+  .text-code2 {
+    width: 25%;
+    margin: auto;
+    margin-top: 25px;
+  }
+  .text-code2 img {
+    width: 100%;
+  }
+  .flex-cheikho.cl .div-ch:hover {
+    transition: 0.6s ease-in;
+    transform: skewY(4deg);
+  }
+  .ads img {
+    width: 100%;
+    border-radius: 20px;
+  }
+  .ads {
+    width: 95%;
+    margin: auto;
+  }
+  .ouroude {
+    background: #40798c80;
+    margin: 30px 0px;
+    padding-bottom: 25px;
+  }
+  .boxour a.adrd img {
+    width: 100%;
+  }
+  .boxour {
+    width: 98%;
+    margin: auto;
+    display: flex;
+  }
+  .boxour a.adrd {
+    margin: 1.5%;
+  }
+  .textani img {
+    width: 250px;
+  }
+  .textani {
+    padding: 10px;
+    float: right;
+    position: relative;
+    margin-top: 10px;
+  }
+  .textani:before {
+    content: " ";
+    display: block;
+    width: 30px;
+    height: 30px;
+    position: absolute;
+    background: url(https://www14.0zz0.com/2022/08/25/14/738910648.png)
+      no-repeat;
+    left: -26px;
+    top: 6px;
+    background-size: cover;
+    background-position: center;
+    animation: cheikho 1.6s ease-in infinite;
+  }
+  @keyframes cheikho {
+    0%,
+    100% {
+      transform: rotate(-10deg);
+    }
+    50% {
+      transform: rotate(210deg);
+    }
+  }
+  .grid2 {
+    grid-template-columns: repeat(5, 1fr);
+  }
+  .grid3 {
+    grid-template-columns: repeat(6, 1fr);
+  }
+  .grid4 {
+    grid-template-columns: repeat(7, 1fr);
+  }
+</style>
+<div class="text-code">
+  <img src="https://www4.0zz0.com/2022/09/06/20/254347852.png" alt="" />
+</div>
+<div class="box-cheikho grid3">
+    <span>
+        <a href="https://ravella2.com/%D8%AA%D8%B3%D9%84%D9%8A%D9%85-%D9%81%D9%88%D8%B1%D9%8A-%D8%AE%D9%84%D8%A7%D9%84-2-5-%D8%A7%D9%8A%D8%A7%D9%85/c2145510893"
+          ><img src="https://www6.0zz0.com/2022/09/06/18/645688686.png" />
+        </a>
+      </span>
+
+  <span>
+    <a href="https://ravella2.com/%D9%81%D8%B3%D8%A7%D8%AA%D9%8A%D9%86/c565975171">
+      <img src="https://www6.0zz0.com/2022/09/06/18/714985503.png"
+    /></a>
+  </span>
+
+  <span>
+    <a href="https://ravella2.com/%D9%85%D8%A7%D8%B1%D9%83%D8%A7%D8%AA-%D8%A7%D9%88%D8%B1%D9%88%D8%A8%D9%8A%D8%A9-vip/c192241373">
+      <img src="https://www6.0zz0.com/2022/09/06/18/343873842.png"
+    /></a>
+  </span>
+
+  <span>
+    <a href="https://ravella2.com/%D8%AA%D9%8A%D9%88%D8%B1%D8%A7%D8%AA/c1096204187">
+      <img src="https://www6.0zz0.com/2022/09/06/18/786794583.png"
+    /></a>
+  </span>
+
+  <span>
+    <a href="https://ravella2.com/%D8%A8%D8%AF%D9%84%D8%A7%D8%AA/c1830354405">
+      <img src="https://www6.0zz0.com/2022/09/06/18/168522919.png"
+    /></a>
+  </span>
+  <span>
+    <a href="https://ravella2.com/%D8%A8%D9%84%D8%A7%D9%8A%D8%B2/c2131517286"
+      ><img src="https://www6.0zz0.com/2022/09/06/18/229767953.png" />
+    </a>
+  </span>
+</div>
+`;
+console.log('cheikho code activet 2');
