fc2496af89cc3d5f09b0fdac3cb8676ac61ac9f8
Cheikh Oumou <67443473+cheikhoumou@users.noreply.github.com>
2023-04-01 16:04:56 -0700

    v1
    v1

---
+++ riddle-store.com1.js
@@ -0,0 +1,57 @@
+// code img
+document.querySelector(
+  ".s-block.s-block--photos-slider + section.s-block.s-block--banners"
+).innerHTML = `
+<style>.boxsalla {width: 98%;margin: auto;display: block;}.boxsalla .boxing {display: flex;width: 100%;}.boxing a {width: 33.3%;margin: 8px;}.boxing a img {width: 100%;}section.s-block.s-block--photos-slider + section.s-block.s-block--banners + section.s-block.s-block--banners {display: none;}
+.boxsalla .boxing .linkstop:before {
+    content:"";
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    display: block;
+    border-radius: 6px;
+    background-color: #000000b8;
+    opacity: 0;
+    z-index: 100;
+}a.linkstop {
+    position: relative;
+    overflow: hidden;
+    border-radius: 6px;
+}.boxsalla .boxing .linkstop:hover:before{
+    transition: all 0.4s;
+    opacity: 1;} .linkstop:hover img{
+    transform:scale(1.2);
+    transition: all 0.3s;
+}salla-slider.photos-slider .swiper-slide {
+    width: 100%;
+    margin: auto 11px;
+    padding: 0;
+}</style>
+<div class="boxsalla">
+<div class="boxing">
+  <a class="linkstop" href="https://riddle-store.com/redirect/categories/123901576">
+  <img src="https://cdn.salla.sa/form-builder/vpo9MqkHeKwh4eyfdVEYvHhrSGjs5WurOE7Z4yJa.png">
+  </a>
+   <a class="linkstop" href="https://riddle-store.com/redirect/categories/1770922737">
+  <img src="https://cdn.salla.sa/form-builder/TMHMGF7iNFY6atRYfX3NubUzAXhIaAhz7XHqIOGD.png">
+   </a>
+   <a class="linkstop" href="https://riddle-store.com/redirect/categories/701274744">
+  <img src="https://cdn.salla.sa/form-builder/MUu7PDUkzktCHIslWwXtLr5yenkn2O9JbeZfWIDG.png">
+   </a>
+  </div>
+  <div class="boxing">
+   <a class="linkstop" href="https://riddle-store.com/redirect/categories/1753944227">
+  <img src="https://cdn.salla.sa/form-builder/11SwnaDrfzxGSSO39VRZzE2PLK0StE7Yy8MmrgqD.png">
+   </a>
+   <a class="linkstop" href="https://riddle-store.com/redirect/categories/902942546">
+  <img src="https://cdn.salla.sa/form-builder/ElazKGLVYEtZnrZioxkHpH3Nlw5ppWoceIBKuPxq.png">
+   </a><a class="linkstop" href="https://riddle-store.com/redirect/categories/593505373">
+  <img src="https://cdn.salla.sa/form-builder/Dc0s3NfvfM4xKHdJ9GwDRP4KK9ielbnvtSI81VdI.png"></a></div></div>`;
+
+document.querySelector(
+  "a.footer-logo img"
+).src = `	https://e.top4top.io/p_261996pp61.gif`;
+
+//code img
