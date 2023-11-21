3b9e570a12e1c994fb93287b626efe71c5adaa48
Cheikh Oumou <67443473+cheikhoumou@users.noreply.github.com>
2023-11-21 07:30:39 -0800

    Update topjs.js

--- topjs.js
+++ topjs.js
@@ -1,4 +1,82 @@
-
+document.querySelector("body > section:nth-child(5)").innerHTML = `
+<style>.box {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    justify-content: center;
+    align-items: center;
+}.box .mb {
+    width: 98%;
+    margin: auto;
+}.box .mb img {
+    width: 100%;
+}@keyframes rotateIn {
+    0% {
+        transform-origin: center;
+        -webkit-transform: rotate(-200deg);
+        transform: rotate(-200deg);
+        opacity: 0
+    }0%,to {
+        -webkit-transform-origin: center
+    }to {
+        transform-origin: center;
+        -webkit-transform: none;
+        transform: none;
+        opacity: 1
+    }
+}.kism:before {
+    position: absolute;
+    content: "";
+    top: 30%;
+    width: 14px;
+    height: 14px;
+    display: block;
+    right: -19px;
+    margin-top: 3px;
+    -webkit-animation: rotateIn 3s infinite;
+    -moz-animation: rotateIn 3s infinite;
+    box-shadow: 3px 3px 0px 0px #f49f04;
+    border-radius: 4px;
+}.kism:before {
+    background: #1b2b4d !important;
+}.kism {position: relative;
+    position: relative;
+    margin-right: 38px;
+    color: #1b2b4d;
+    font-family: inherit;
+    font-weight: bold;
+    font-size: 23px;
+}
+</style>
+<div class="kism">
+    المنتجات الرقمية
+</div>
+<div class="box">
+    <div class="b1 mb"><a href="https://cardsworldsa.com/%D8%B4%D8%AF%D8%A7%D8%AA-%D8%A8%D8%A8%D8%AC%D9%8A/c1392673791" ><img src="https://www3.0zz0.com/2023/11/21/14/972347677.jpeg" alt=""></a></div>
+    <div class="b2 mb"><a href="https://cardsworldsa.com/%D8%B4%D8%AF%D8%A7%D8%AA-%D8%A8%D8%A8%D8%AC%D9%8A-%D9%86%D8%B4%D8%AD%D9%86%D9%87%D8%A7-%D9%84%D9%83/c555033375" ><img src="https://www4.0zz0.com/2023/11/21/14/383365163.jpeg" alt=""></a></div>
+    <div class="b3 mb"><a href="https://cardsworldsa.com/%D9%85%D8%AC%D9%88%D9%87%D8%B1%D8%A7%D8%AA-%D8%A7%D8%B2%D8%A7%D8%B1/c444469243" ><img src="https://www4.0zz0.com/2023/11/21/14/850279281.jpeg" alt=""></a></div>
+    <div class="b4 mb"><a href="https://cardsworldsa.com/%D9%85%D8%AC%D9%88%D9%87%D8%B1%D8%A7%D8%AA-%D8%A7%D8%B2%D8%A7%D8%B1-%D9%86%D8%B4%D8%AD%D9%86-%D8%AD%D8%B3%D8%A7%D8%A8%D9%83/c1176522181" ><img src="https://www4.0zz0.com/2023/11/21/14/910555278.jpeg" alt=""></a></div>
+    <div class="b5 mb"><a href="https://cardsworldsa.com/%D8%A8%D8%A7%D9%82%D8%A7%D8%AA-%D8%B3%D9%88%D8%A7/c269315270" ><img src="https://www4.0zz0.com/2023/11/21/14/647339697.jpeg" alt=""></a></div>
+    <div class="b6 mb"><a href="https://cardsworldsa.com/%D8%B9%D9%85%D9%84%D8%A7%D8%AA-%D8%AA%D9%8A%D9%83-%D8%AA%D9%88%D9%83/c1844153287" ><img src="https://www10.0zz0.com/2023/11/21/14/194898241.jpeg" alt=""></a></div>
+    <div class="b7 mb"><a href="https://cardsworldsa.com/%D9%8A%D9%84%D8%A7-%D9%84%D9%88%D8%AF%D9%88-%D8%A7%D9%83%D9%88%D8%A7%D8%AF/c1068542656" ><img src="https://www10.0zz0.com/2023/11/21/14/657613850.jpeg" alt=""></a></div>
+    <div class="b8 mb"><a href="https://cardsworldsa.com/%D9%8A%D9%84%D8%A7-%D9%84%D9%88%D8%AF%D9%88-%D8%AA%D8%B3%D8%AC%D9%8A%D9%84-%D8%AF%D8%AE%D9%88%D9%84/c1534781634" ><img src="https://www10.0zz0.com/2023/11/21/14/593956032.jpeg" alt=""></a></div>
+    <div class="b9 mb"><a href="https://cardsworldsa.com/%D8%A7%D8%B4%D8%AA%D8%B1%D8%A7%D9%83%D8%A7%D8%AA-%D8%B4%D8%A7%D9%87%D8%AF/c120329932" ><img src="https://www10.0zz0.com/2023/11/21/14/874355915.jpeg" alt=""></a></div>
+    <div class="b10 mb"><a href="https://cardsworldsa.com/%D8%A7%D8%B4%D8%AA%D8%B1%D8%A7%D9%83%D8%A7%D8%AA-%D9%86%D8%AA%D9%81%D9%84%D9%83%D8%B3/c584471758" ><img src="https://www10.0zz0.com/2023/11/21/14/908251233.jpeg" alt=""></a></div>
+    <div class="b11 mb"><a href="https://cardsworldsa.com/%D8%A7%D8%B4%D8%AA%D8%B1%D8%A7%D9%83%D8%A7%D8%AA-%D8%A8%D9%84%D8%B3/c1318687432" ><img src="https://www10.0zz0.com/2023/11/21/14/843524618.jpeg" alt=""></a></div>
+    <div class="b12 mb"><a href="" ><img src="" alt=""></a></div>
+</div>
+<div class="kism">
+    خدمات التسويق
+</div>
+<div class="box">
+    <div class="b1 mb"><a href="https://cardsworldsa.com/%D8%A7%D9%86%D8%B3%D8%AA%D8%BA%D8%B1%D8%A7%D9%85/c34756259" ><img src="https://www6.0zz0.com/2023/11/21/14/903757787.jpeg" alt=""></a></div>
+    <div class="b1 mb"><a href="https://cardsworldsa.com/%D8%AA%D9%8A%D9%83-%D8%AA%D9%88%D9%83/c691432231" ><img src="https://www6.0zz0.com/2023/11/21/14/401040805.jpeg" alt=""></a></div>
+    <div class="b1 mb"><a href="https://cardsworldsa.com/%D8%A3%D9%83%D8%B3/c2065467936" ><img src="https://www6.0zz0.com/2023/11/21/14/628028071.jpeg" alt=""></a></div>
+    <div class="b1 mb"><a href="https://cardsworldsa.com/%D8%B3%D9%86%D8%A7%D8%A8-%D8%B4%D8%A7%D8%AA/c1425647905" ><img src="https://www6.0zz0.com/2023/11/21/14/730765621.jpeg" alt=""></a></div>
+    <div class="b1 mb"><a href="https://cardsworldsa.com/%D8%AA%D9%84%D9%8A%D8%AC%D8%B1%D8%A7%D9%85/c650561570" ><img src="https://www6.0zz0.com/2023/11/21/14/616260766.jpeg" alt=""></a></div>
+    <div class="b1 mb"><a href="https://cardsworldsa.com/%D9%8A%D9%88%D8%AA%D9%8A%D9%88%D8%A8/c1889785635" ><img src="https://www6.0zz0.com/2023/11/21/14/413643357.jpeg" alt=""></a></div>
+    <div class="b1 mb"><a href="https://cardsworldsa.com/%D8%AA%D9%88%D9%8A%D8%AA%D8%B4/c1116272172" ><img src="https://www6.0zz0.com/2023/11/21/14/460406551.jpeg" alt=""></a></div>
+    <div class="b1 mb"><a href="" ><img src="" alt=""></a></div>
+</div>
+`;
 console.log("cheikhoumou salla Dev phone: +222 20523820");
-console.log("web: https://www.cheikhoumou-portfolio.ml/");
 console.log("http://wa.me/22220523820");
