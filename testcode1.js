a254d93c7fd68df604e7c2a867ddf75ab65023b4
Cheikh Oumou <67443473+cheikhoumou@users.noreply.github.com>
2024-05-14 01:03:35 +0200

    Update and rename testcode.js to testcode1.js

---
+++ testcode1.js
@@ -0,0 +1,5 @@
+console.log(' test 1');
+document.querySelector(".mm-spn li:nth-child(1) a").onclick = function() {
+    if (!window.__cfRLUnblockHandlers) return false; salla.event.dispatch('login::open')
+}
+document.querySelector(".mm-spn li:nth-child(1) a").href = "#";
