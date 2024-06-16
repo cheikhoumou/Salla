f105b44dcb297334fd123faf4c278917535f77aa
Cheikh Oumou <67443473+cheikhoumou@users.noreply.github.com>
2024-06-16 15:13:01 +0200

    Update and rename wasit-test1.js to wasit-test2.js

--- wasit-test2.js
+++ wasit-test2.js
@@ -97,10 +97,10 @@ function conditionlink() {
 }
 function seveddata(){
     document.querySelector("div > div.wajiha").style = `animation: fadeout 0.8s ease-in both;display:none;`;
-    document.querySelector(".wp-block-uagb-image--align-center.aos-init.aos-animate > figure > img").style = `animation: fadeout 0.6s ease-in both; display:none;`
     document.querySelector("div.sendbox").style = `display: block;animation: fadeIn 1.3s ease-in both;`;
     document.querySelector("div.sendbox > div.bloking1 > div").style = `transform: scale(0);display: none;`;
     document.querySelector("div.sendbox > div.bloking2").style = `display: block;animation: fadeIn 1.3s ease-in both;`;
+    document.querySelector(".aos-animate > figure > img").style = `animation: fadeout 0.6s ease-in both; display:none;`
     document.querySelector("div.sendbox > div.progriss > span:nth-child(1)").style = `background: #4434c7;`;
     document.querySelector("div.sendbox > div.progriss > span:nth-child(2)").style = `background: #4434c7;`;
     document.querySelector("div.sendbox > div.progriss > span:nth-child(3)").style = `background: #4434c7;`;
