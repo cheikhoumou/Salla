72ff905df911e5b36b05640562af05958731ea2c
Cheikh Oumou <67443473+cheikhoumou@users.noreply.github.com>
2024-06-22 13:28:19 +0200

    Update and rename wasit-test.js to wasit-test1.js

--- wasit-test1.js
+++ wasit-test1.js
@@ -154,6 +154,7 @@ if (localStorage.getItem("Name") == null) {
     document.querySelector('.addinput').style = `display: none;`;
     document.querySelector('.seve').style = `display: none;`;
     document.querySelector('.sendid').style = `display: block;`;
+    document.querySelector(".sendbox > div.bloking2 > div > div > a").style = `display: block;`;
     // document.querySelector(".contener > div > span").style = `display: none;`;
     document.querySelector("div.sendbox > div.progriss > span:nth-child(3)").style = `background: #4434c7;`;
     seveddata();
