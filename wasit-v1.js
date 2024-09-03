321bf3743191b2826476d8e33575d4d3848f2699
Cheikh Oumou <67443473+cheikhoumou@users.noreply.github.com>
2024-09-03 22:17:30 +0200

    Update and rename wasit.js to wasit-v1.js

--- wasit-v1.js
+++ wasit-v1.js
@@ -1,4 +1,25 @@
 // document.querySelectorAll('figure.wp-block-uagb-image__figure')[0].classList.add('checkbox');
+let prodacts = [
+    document.querySelector(".wp-block-woocommerce-all-products.wc-block-all-products.prodactswocom > div.wc-block-grid.has-4-columns.has-multiple-rows > ul > li:nth-child(1) > div.wp-block-button.wc-block-components-product-button.wc-block-grid__product-add-to-cart > button"),
+    document.querySelector(".wp-block-woocommerce-all-products.wc-block-all-products.prodactswocom > div.wc-block-grid.has-4-columns.has-multiple-rows > ul > li:nth-child(2) > div.wp-block-button.wc-block-components-product-button.wc-block-grid__product-add-to-cart > button"),
+    document.querySelector(".wp-block-woocommerce-all-products.wc-block-all-products.prodactswocom > div.wc-block-grid.has-4-columns.has-multiple-rows > ul > li:nth-child(3) > div.wp-block-button.wc-block-components-product-button.wc-block-grid__product-add-to-cart > button"),
+    document.querySelector(".wp-block-woocommerce-all-products.wc-block-all-products.prodactswocom > div.wc-block-grid.has-4-columns.has-multiple-rows > ul > li:nth-child(4) > div.wp-block-button.wc-block-components-product-button.wc-block-grid__product-add-to-cart > button"),
+    document.querySelector(".wp-block-woocommerce-all-products.wc-block-all-products.prodactswocom > div.wc-block-grid.has-4-columns.has-multiple-rows > ul > li:nth-child(5) > div.wp-block-button.wc-block-components-product-button.wc-block-grid__product-add-to-cart > button"),
+    document.querySelector(".wp-block-woocommerce-all-products.wc-block-all-products.prodactswocom > div.wc-block-grid.has-4-columns.has-multiple-rows > ul > li:nth-child(6) > div.wp-block-button.wc-block-components-product-button.wc-block-grid__product-add-to-cart > button"),
+    document.querySelector(".wp-block-woocommerce-all-products.wc-block-all-products.prodactswocom > div.wc-block-grid.has-4-columns.has-multiple-rows > ul > li:nth-child(7) > div.wp-block-button.wc-block-components-product-button.wc-block-grid__product-add-to-cart > button"),
+    document.querySelector(".wp-block-woocommerce-all-products.wc-block-all-products.prodactswocom > div.wc-block-grid.has-4-columns.has-multiple-rows > ul > li:nth-child(8) > div.wp-block-button.wc-block-components-product-button.wc-block-grid__product-add-to-cart > button"),
+    document.querySelector(".wp-block-woocommerce-all-products.wc-block-all-products.prodactswocom > div.wc-block-grid.has-4-columns.has-multiple-rows > ul > li:nth-child(9) > div.wp-block-button.wc-block-components-product-button.wc-block-grid__product-add-to-cart > button"),
+    document.querySelector(".wp-block-woocommerce-all-products.wc-block-all-products.prodactswocom > div.wc-block-grid.has-4-columns.has-multiple-rows > ul > li:nth-child(10) > div.wp-block-button.wc-block-components-product-button.wc-block-grid__product-add-to-cart > button"),
+    document.querySelector(".wp-block-woocommerce-all-products.wc-block-all-products.prodactswocom > div.wc-block-grid.has-4-columns.has-multiple-rows > ul > li:nth-child(11) > div.wp-block-button.wc-block-components-product-button.wc-block-grid__product-add-to-cart > button"),
+    document.querySelector(".wp-block-woocommerce-all-products.wc-block-all-products.prodactswocom > div.wc-block-grid.has-4-columns.has-multiple-rows > ul > li:nth-child(12) > div.wp-block-button.wc-block-components-product-button.wc-block-grid__product-add-to-cart > button"),
+    document.querySelector(".wp-block-woocommerce-all-products.wc-block-all-products.prodactswocom > div.wc-block-grid.has-4-columns.has-multiple-rows > ul > li:nth-child(13) > div.wp-block-button.wc-block-components-product-button.wc-block-grid__product-add-to-cart > button"),
+    document.querySelector(".wp-block-woocommerce-all-products.wc-block-all-products.prodactswocom > div.wc-block-grid.has-4-columns.has-multiple-rows > ul > li:nth-child(14) > div.wp-block-button.wc-block-components-product-button.wc-block-grid__product-add-to-cart > button"),
+    document.querySelector(".wp-block-woocommerce-all-products.wc-block-all-products.prodactswocom > div.wc-block-grid.has-4-columns.has-multiple-rows > ul > li:nth-child(15) > div.wp-block-button.wc-block-components-product-button.wc-block-grid__product-add-to-cart > button"),
+    document.querySelector(".wp-block-woocommerce-all-products.wc-block-all-products.prodactswocom > div.wc-block-grid.has-4-columns.has-multiple-rows > ul > li:nth-child(16) > div.wp-block-button.wc-block-components-product-button.wc-block-grid__product-add-to-cart > button"),
+    document.querySelector(".wp-block-woocommerce-all-products.wc-block-all-products.prodactswocom > div.wc-block-grid.has-4-columns.has-multiple-rows > ul > li:nth-child(17) > div.wp-block-button.wc-block-components-product-button.wc-block-grid__product-add-to-cart > button"),
+    document.querySelector(".wp-block-woocommerce-all-products.wc-block-all-products.prodactswocom > div.wc-block-grid.has-4-columns.has-multiple-rows > ul > li:nth-child(18) > div.wp-block-button.wc-block-components-product-button.wc-block-grid__product-add-to-cart > button"),
+    document.querySelector(".wp-block-woocommerce-all-products.wc-block-all-products.prodactswocom > div.wc-block-grid.has-4-columns.has-multiple-rows > ul > li:nth-child(19) > div.wp-block-button.wc-block-components-product-button.wc-block-grid__product-add-to-cart > button")
+];
 let ba9att = [
     document.querySelectorAll('figure.wp-block-uagb-image__figure')[0],
     document.querySelectorAll('figure.wp-block-uagb-image__figure')[1],
@@ -446,6 +467,65 @@ document.querySelector(".block1 .uagb-buttons-repeater.wp-block-button__link").o
        console.log(' !!! لم يختر باقة  ');
     }else{
     //  جاري الارسال ...
+    let B = localStorage.getItem("ba9");
+    switch (B) {
+        case '100':
+            prodacts[0].click();
+            break;
+        case '200':
+            prodacts[1].click();
+            break;
+        case '300':
+            prodacts[2].click();
+            break;
+        case '400':
+            prodacts[3].click();
+            break;
+        case '500':
+            prodacts[4].click();
+            break;
+        case '600':
+            prodacts[5].click();
+            break;
+        case '700':
+            prodacts[6].click();
+            break;
+        case '800':
+            prodacts[7].click();
+            break;
+        case '900':
+            prodacts[8].click();
+            break;
+        case '1200':
+            prodacts[9].click();
+            break;
+        case '1500':
+            prodacts[10].click();
+            break;
+        case '1800':
+            prodacts[11].click();
+            break;
+        case '2000':
+            prodacts[12].click();
+            break;
+        case '2100':
+            prodacts[13].click();
+            break;
+        case '2400':
+            prodacts[14].click();
+            break;
+        case '2500':
+            prodacts[15].click();
+            break;
+        case '3000':
+            prodacts[16].click();
+            break;
+        case '3500':
+            prodacts[17].click();
+            break;
+        case '4000':
+            lprodacts[18].click();
+            break;}
     document.querySelector(".block1 .uagb-buttons-repeater.wp-block-button__link .uagb-button__link").innerHTML = 'جاري الارسال ...';
     document.querySelector("div.sendid > p").innerHTML = " اكمل طلب " + localStorage.getItem("ba9");
     document.querySelector("a.uagb-buttons-repeater.wp-block-button__link img").style = `display:block;`;
@@ -466,6 +546,8 @@ document.querySelector("div.clsanimated2").onclick = function(){
      document.querySelector(".block1 .uagb-buttons-repeater.wp-block-button__link .uagb-button__link").innerHTML = ' تقديم الطلب ';
      document.querySelector("a.uagb-buttons-repeater.wp-block-button__link img").style = `display:none;`;
      document.querySelector("div.sendbox > div.bloking2").style = `display:none;`;
+     // code remove prodact
+     document.querySelector(".wp-block-woocommerce-cart > div:nth-child(4) > div > div > div.wc-block-components-main.wc-block-cart__main.wp-block-woocommerce-cart-items-block > table > tbody > tr > td.wc-block-cart-item__product > div > div.wc-block-cart-item__quantity > button").click();
 }
 // open window form
 document.querySelector("div.sendbox > div.bloking1 > div > button").onclick = function(){
